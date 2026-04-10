546615e1f102d08647a918c476921f373f01cc4b
sslcristian <catencia1@udi.edu.co>
2026-04-09 21:23:52 -0500

    AVANCE
    GRANDES CAMBIOS Y EMPEZAMOS CON EL CODIGO

--- models/player_model.gd
+++
@@ -1,8 +0,0 @@
-extends RefCounted
-class_name PlayerModel
-
-var id
-var powers = []
-
-func _init(p_id):
-	id = p_id
--- views/cell_view.gd
+++
@@ -1,7 +0,0 @@
-extends Button
-class_name CellView
-
-signal clicked
-
-func _pressed():
-	emit_signal("clicked")
